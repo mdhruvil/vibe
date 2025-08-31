72444c7eacf6226ba629fa66ee5a94fcdd06783f
mdhruvil <dhruvil1808@gmail.com>
2025-08-31 13:22:41 +0530

    chore: better t stack

---
+++ apps/server/src/lib/auth.ts
@@ -0,0 +1,26 @@
+import { env } from 'cloudflare:workers';
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { db } from '../db';
+import * as schema from '../db/schema/auth';
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: 'sqlite',
+
+    schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  secret: env.BETTER_AUTH_SECRET,
+  baseURL: env.BETTER_AUTH_URL,
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: 'none',
+      secure: true,
+      httpOnly: true,
+    },
+  },
+});
