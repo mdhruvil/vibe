f43c2e5ed0d1398bbbe2d62f3a68e8ee9e92100c
mdhruvil <dhruvil1808@gmail.com>
2025-09-01 16:37:47 +0530

    feat: basic vm next js

--- apps/dash/next.config.ts
+++ apps/dash/next.config.ts
@@ -1,7 +1,24 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // biome-ignore lint/suspicious/useAwait: <next config expects promise>
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
---
+++ apps/dash/src/stores/vm.ts
@@ -0,0 +1,53 @@
+import { WebContainer, type WebContainerProcess } from '@webcontainer/api';
+import { create } from 'zustand';
+
+type VMStore = {
+  vm: WebContainer | null;
+  vmStatus: 'idle' | 'booting' | 'installing' | 'running' | 'stopped' | 'error';
+  initVM: () => Promise<void>;
+  shutdownVM: () => void;
+  runCommand: (
+    command: string,
+    args?: string[]
+  ) => Promise<WebContainerProcess>;
+};
+
+export const useVMStore = create<VMStore>()((set, get) => ({
+  vm: null,
+  vmStatus: 'idle',
+  initVM: async () => {
+    const state = get();
+    if (state.vm) {
+      console.log('[VM] VM is already up');
+      return;
+    }
+    set({ vmStatus: 'booting' });
+    console.log('[VM] Booting the vm...');
+    try {
+      const vmInstance = await WebContainer.boot();
+      set({ vm: vmInstance, vmStatus: 'running' });
+      console.log('[VM] VM is running');
+    } catch (error) {
+      console.log('[VM] Error while booting the vm');
+      console.log(error);
+      set({ vmStatus: 'error' });
+    }
+  },
+  shutdownVM: () => {
+    const state = get();
+    if (state.vm) {
+      state.vm.teardown();
+      set({ vm: null, vmStatus: 'stopped' });
+      console.log('[VM] VM has been stopped');
+    }
+  },
+  runCommand: async (command, args) => {
+    const state = get();
+    if (!state.vm) {
+      throw new Error('VM is not initialized');
+    }
+
+    const process = await state.vm.spawn(command, args ?? []);
+    return process;
+  },
+}));
